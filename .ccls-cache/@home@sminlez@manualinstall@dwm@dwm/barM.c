a1a54fc5868948d6b421ea17fedeb3f9272dc712
Sminlez V2 <sminlezv2@gmail.com>
2021-01-07 16:07:08 +0100

    Modified config.h

--- .ccls-cache/@home@sminlez@manualinstall@dwm@dwm/barM.c
+++ .ccls-cache/@home@sminlez@manualinstall@dwm@dwm/barM.c
@@ -88,7 +88,7 @@ static const char * ram(void){
         static char ram[MAXSTR];
         struct sysinfo s;
         sysinfo(&s);
-        snprintf(ram,sizeof(ram),"%.1fM,%.1fM",((double)(s.totalram-s.freeram))/1048576.,((double)s.totalram)/1048576.);
+        snprintf(ram,sizeof(ram),"%.1fM,%.1fM",((double)(s.totalram-s.freeram))/1048576./2,((double)s.totalram)/1048576./2);
         return ram;
 }
 
--- .ccls-cache/@home@sminlez@manualinstall@dwm@dwm/config.h
+++ .ccls-cache/@home@sminlez@manualinstall@dwm@dwm/config.h
@@ -5,13 +5,13 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Space Mono For Powerline:size=10" };
+static const char dmenufont[]       = "Space Mono For Powerline:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#00ae86";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "xterm", NULL };
+static const char *termcmd[]  = { "/usr/local/bin/st", NULL };
 static const char *upvol[]   = { "/usr/bin/amixer", "-D", "pulse", "sset","Master","5%+", NULL };
 static const char *downvol[]   = { "/usr/bin/amixer", "-D", "pulse", "sset","Master","5%-", NULL };
 static const char *mutevol[]   = { "/usr/bin/amixer", "-D", "pulse", "sset","Master","100%-", NULL };
